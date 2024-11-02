2b9da1a938639d8212ad7af886e84a7dac7b5f87
Tamim Haque <168730535+tamim-2024@users.noreply.github.com>
2024-11-03 01:17:15 +0600

    Add files via upload

--- 299/database/digitaldemocracy.sql
+++ 299/database/digitaldemocracy.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2024 at 06:19 PM
+-- Generation Time: Nov 02, 2024 at 08:17 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,15 +33,16 @@ CREATE TABLE `administrator` (
   `fname` varchar(20) DEFAULT NULL,
   `lname` varchar(20) DEFAULT NULL,
   `password` varchar(20) DEFAULT NULL,
-  `confirm_password` varchar(20) DEFAULT NULL
+  `confirm_password` varchar(20) DEFAULT NULL,
+  `result` tinyint(1) NOT NULL DEFAULT 0 CHECK (`result` in (0,1))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `administrator`
 --
 
-INSERT INTO `administrator` (`id`, `admin_id`, `fname`, `lname`, `password`, `confirm_password`) VALUES
-(1, 2121146042, 'Tamim', 'Haque', '1234ab', '1234ab');
+INSERT INTO `administrator` (`id`, `admin_id`, `fname`, `lname`, `password`, `confirm_password`, `result`) VALUES
+(1, 2121146042, 'Tamim', 'Haque', '1234ab', '1234ab', 1);
 
 -- --------------------------------------------------------
 
@@ -65,12 +66,15 @@ CREATE TABLE `candidate` (
 --
 
 INSERT INTO `candidate` (`id`, `nid_no`, `fname`, `lname`, `ward`, `city`, `profile_picture`, `vote_count`) VALUES
-(1, '1234567890', 'John', 'Doe', '3', 'Dhaka', NULL, 1),
-(2, '2121146642', 'Yamim', 'Haque', '3', 'DhakA', 'uploads\\1728331423915.jpg', 0),
-(8, '2121146042', 'QAMIM', 'HAQUE', '3', 'Dhaka', 'uploads\\1728844927030.jpg', 1),
-(10, '2121146542', 'tamim', 'haque', '3', 'Dhaka', 'uploads\\1728898413163.jpg', 1),
+(1, '1234567890', 'John', 'Doe', '3', 'Dhaka', NULL, 0),
+(2, '2121146642', 'Yamim', 'Haque', '3', 'DhakA', 'uploads\\1728331423915.jpg', 5),
+(8, '2121146042', 'QAMIM', 'HAQUE', '3', 'Dhaka', 'uploads\\1728844927030.jpg', 0),
+(10, '2121146542', 'tamim', 'haque', '3', 'Dhaka', 'uploads\\1728898413163.jpg', 2),
 (12, '2121146842', 'TAMIM', 'HASAN', '2', 'Dhaka', 'uploads\\1728920414245.jpg', 0),
-(13, '2121146942', 'RAFAT', 'HAQUE', '2', 'Dhaka', 'uploads\\1728920500234.jpg', 1);
+(13, '2121146942', 'RAFAT', 'HAQUE', '2', 'Dhaka', 'uploads\\1728920500234.jpg', 1),
+(14, '2121146742', 'EAMIM', 'HASAN', '3', 'Dhaka', 'uploads\\1728929536649.jpg', 4),
+(15, '2131748043', 'SUDIPTO', 'SARKER', '3', 'Dhaka', 'uploads\\1728969873457.jpg', 5),
+(16, '2121146142', 'QAMIM', 'AHMED', '3', 'Dhaka', 'uploads\\1730569786848.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -98,26 +102,28 @@ CREATE TABLE `voter` (
   `password` varchar(255) DEFAULT NULL,
   `profile_picture` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `phone_number` varchar(11) DEFAULT NULL
+  `phone_number` varchar(11) DEFAULT NULL,
+  `vote_given` tinyint(1) NOT NULL DEFAULT 0 CHECK (`vote_given` in (0,1))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `voter`
 --
 
-INSERT INTO `voter` (`id`, `fname`, `lname`, `email`, `gender`, `father_name`, `mother_name`, `nid_no`, `dob`, `blood_group`, `home`, `ward`, `post_office`, `postal_code`, `city`, `country`, `password`, `profile_picture`, `created_at`, `phone_number`) VALUES
-(1, 'grsgf', 'frvsrf', 'sefdaf2gmail.com', 'male', 'dfvfd', 'fvsxzv', 2121146, '2024-10-16', 'O+', 0, '0', 'dvsz', 0, 'Dhaka', 'dv', '$2b$10$6.e7yAm6TrPyTMoA05clpuavjdY4g1bKQf74QPzJ9VYze9OKAc6zq', NULL, '2024-10-04 19:44:15', NULL),
-(2, 'bthdtc', 'dfgbh', 'dfgv', 'male', 'fgv', 'fgzv', 2147483647, '2024-10-13', 'O+', 3, '3', 'sfzvg', 1207, 'Dhaka', 'vgxdf', '$2b$10$N/7U/Uwo6o5aKyQnqK0XFeiVRUcB1hEYIbYtrkp4xMO0sBLqhYLpy', NULL, '2024-10-04 20:18:01', NULL),
-(16, 'John', 'Doe', 'john@example.com', 'male', 'Father Name', 'Mother Name', 1234567890123, '1990-01-01', 'A+', 12, '5', 'Post Office', 1234, 'Dhaka', 'Bangladesh', 'hashedPassword', 'uploads/12345.jpg', '2024-10-05 07:28:13', '01234567890'),
-(20, 'gfbn', 'gb', 'bgh', 'male', 'gdb', 'fgb', 2121146742, '2024-10-21', 'O+', 3, '3', 'df', 1207, 'Dhaka', 'dfgh', '$2b$10$rJPY4W8pbwuRyh4wzSoiL.Oo8VG8y/JFrJkYq5ROLLoSX/9dYhk2e', 'uploads\\1728120989439.jpg', '2024-10-05 09:36:29', '01929487006'),
-(21, 'gffdxg', 'fxg', 'fxg', 'male', 'fgx', 'fxg', 2121146042205, '2024-10-24', 'O+', 3, '3', 'dzxv', 1207, 'Dhaka', 'sdzf', '$2b$10$xfCA4qH2ZhsmmgOx88XaQumdghqH101VTc79ctqgDAGRj/6E6qjfO', 'uploads\\1728152596327.jpg', '2024-10-05 09:37:39', '01929487007'),
-(24, 'TAMIM', 'HAQUE', 'dfsd', 'male', 'sdgvs', 'sdz', 2121146042202, '2024-10-05', 'O+', 3, '3', 'gf', 1207, 'Dhaka', 'bng', '$2b$10$GDFuYhblrqU4tuhAxNCaiOzu31WdOlTNsCxBc1ByPERf.H50LwMg6', 'uploads\\1728152596327.jpg', '2024-10-05 18:23:16', '01929487008'),
-(25, 'fxb', 'xcb x', 'xvcb ', 'male', 'xvb ', 'vx', 2121146042207, '2024-10-02', 'O+', 22, '2', '222', 1234, 'Dhaka', 'sdg', '$2b$10$gMl5.Djg1LsqjlELz7TgvuICpR0.tawF1w/bRiJPfsv30aVSosCw6', 'C:\\Users\\USER\\Desktop\\Digital_Democracy\\backend\\uploads\\1728152746115.jpg', '2024-10-05 18:25:46', '01932778621'),
-(34, 'fgf', 'bfx', 'fxb', 'male', 'fbx', 'fbx', 1234567891011, '2024-10-10', 'O+', 3, '3', '33', 1234, 'Dhaka', 'rtfy', '$2b$10$j3ZG3tP/uYZ0TqR8qzwq4eHy8jc94gHSAa9OmDO7ZF3FgoiO/FI0C', 'C:\\Users\\USER\\Desktop\\Digital_Democracy\\backend\\uploads\\1728153031702.jpg', '2024-10-05 18:30:31', '01932778628'),
-(35, 'rsgv', 'fgxd', 'fxdb', 'male', 'sdvs', 'dvx', 2121146042200, '2024-10-05', 'AB-', 3, '3', '3333', 3333, 'Dhaka', '3333', '$2b$10$3tE0sRSUoHEsD5m4/iXCW.bkaJo3yLKnCTq12sjXV/Z3IMIWRSddu', 'C:\\Users\\USER\\Desktop\\Digital_Democracy\\backend\\uploads\\1728153687884.jpg', '2024-10-05 18:41:27', '01929487000'),
-(36, 'sfgv', 'fgsxz', 'sfxvsx', 'male', 'sZDC', 'dzsV', 2121146042209, '2024-10-02', 'A-', 3, '3', '3333', 3333, 'Dhaka', '333', '$2b$10$15GRV4uF4m3TNPaEz4ukAu6kzLO1dqEEdQDAcbLivNeofQwv.v2z.', 'C:\\Users\\USER\\Desktop\\Digital_Democracy\\backend\\uploads\\1728153839292.jpg', '2024-10-05 18:43:59', '01932778622'),
-(41, 'Hamim', 'haque', 'srfgsz', 'male', 'ghbxd', 'dfgdx', 2121146542, '2024-10-02', 'AB+', 3, '3', 'bmb', 1207, 'Dhaka', 'vgnc', '$2b$10$afGlnmoc5l0gO6ZhtfAo4uq0juSiem3b3.lKDTAiZTZUa42AC/hAm', 'uploads\\1728324903667.jpg', '2024-10-07 18:15:03', '01813075310'),
-(42, 'TAMIM', 'HAQUE', 'tamim@gmail.com', 'male', 'muxa', 'sul', 2121146042, '2024-09-01', 'O+', 3, '3', 'mirpur', 1207, 'Dhaka', 'bng', '$2b$10$a60AzoIXcaR24bgUVkiBWOBWobWAlk0GPLSXRK7ed8VlSsy5DzwAW', 'uploads\\1728838202268.jpg', '2024-10-13 16:50:02', '01813074310');
+INSERT INTO `voter` (`id`, `fname`, `lname`, `email`, `gender`, `father_name`, `mother_name`, `nid_no`, `dob`, `blood_group`, `home`, `ward`, `post_office`, `postal_code`, `city`, `country`, `password`, `profile_picture`, `created_at`, `phone_number`, `vote_given`) VALUES
+(1, 'grsgf', 'frvsrf', 'sefdaf2gmail.com', 'male', 'dfvfd', 'fvsxzv', 2121146, '2024-10-16', 'O+', 0, '0', 'dvsz', 0, 'Dhaka', 'dv', '$2b$10$6.e7yAm6TrPyTMoA05clpuavjdY4g1bKQf74QPzJ9VYze9OKAc6zq', NULL, '2024-10-04 19:44:15', NULL, 0),
+(2, 'bthdtc', 'dfgbh', 'dfgv', 'male', 'fgv', 'fgzv', 2147483647, '2024-10-13', 'O+', 3, '3', 'sfzvg', 1207, 'Dhaka', 'vgxdf', '$2b$10$N/7U/Uwo6o5aKyQnqK0XFeiVRUcB1hEYIbYtrkp4xMO0sBLqhYLpy', NULL, '2024-10-04 20:18:01', NULL, 0),
+(16, 'John', 'Doe', 'john@example.com', 'male', 'Father Name', 'Mother Name', 1234567890123, '1990-01-01', 'A+', 12, '5', 'Post Office', 1234, 'Dhaka', 'Bangladesh', 'hashedPassword', 'uploads/12345.jpg', '2024-10-05 07:28:13', '01234567890', 0),
+(20, 'gfbn', 'gb', 'bgh', 'male', 'gdb', 'fgb', 2121146742, '2024-10-21', 'O+', 3, '3', 'df', 1207, 'Dhaka', 'dfgh', '$2b$10$rJPY4W8pbwuRyh4wzSoiL.Oo8VG8y/JFrJkYq5ROLLoSX/9dYhk2e', 'uploads\\1728120989439.jpg', '2024-10-05 09:36:29', '01929487006', 0),
+(21, 'gffdxg', 'fxg', 'fxg', 'male', 'fgx', 'fxg', 2121146042205, '2024-10-24', 'O+', 3, '3', 'dzxv', 1207, 'Dhaka', 'sdzf', '$2b$10$xfCA4qH2ZhsmmgOx88XaQumdghqH101VTc79ctqgDAGRj/6E6qjfO', 'uploads\\1728152596327.jpg', '2024-10-05 09:37:39', '01929487007', 0),
+(25, 'fxb', 'xcb x', 'xvcb ', 'male', 'xvb ', 'vx', 2121146042207, '2024-10-02', 'O+', 22, '2', '222', 1234, 'Dhaka', 'sdg', '$2b$10$gMl5.Djg1LsqjlELz7TgvuICpR0.tawF1w/bRiJPfsv30aVSosCw6', 'C:\\Users\\USER\\Desktop\\Digital_Democracy\\backend\\uploads\\1728152746115.jpg', '2024-10-05 18:25:46', '01932778621', 0),
+(34, 'fgf', 'bfx', 'fxb', 'male', 'fbx', 'fbx', 1234567891011, '2024-10-10', 'O+', 3, '3', '33', 1234, 'Dhaka', 'rtfy', '$2b$10$j3ZG3tP/uYZ0TqR8qzwq4eHy8jc94gHSAa9OmDO7ZF3FgoiO/FI0C', 'C:\\Users\\USER\\Desktop\\Digital_Democracy\\backend\\uploads\\1728153031702.jpg', '2024-10-05 18:30:31', '01932778628', 0),
+(35, 'rsgv', 'fgxd', 'fxdb', 'male', 'sdvs', 'dvx', 2121146042200, '2024-10-05', 'AB-', 3, '3', '3333', 3333, 'Dhaka', '3333', '$2b$10$3tE0sRSUoHEsD5m4/iXCW.bkaJo3yLKnCTq12sjXV/Z3IMIWRSddu', 'C:\\Users\\USER\\Desktop\\Digital_Democracy\\backend\\uploads\\1728153687884.jpg', '2024-10-05 18:41:27', '01929487000', 0),
+(36, 'sfgv', 'fgsxz', 'sfxvsx', 'male', 'sZDC', 'dzsV', 2121146042209, '2024-10-02', 'A-', 3, '3', '3333', 3333, 'Dhaka', '333', '$2b$10$15GRV4uF4m3TNPaEz4ukAu6kzLO1dqEEdQDAcbLivNeofQwv.v2z.', 'C:\\Users\\USER\\Desktop\\Digital_Democracy\\backend\\uploads\\1728153839292.jpg', '2024-10-05 18:43:59', '01932778622', 0),
+(41, 'Hamim', 'haque', 'srfgsz', 'male', 'ghbxd', 'dfgdx', 2121146542, '2024-10-02', 'AB+', 3, '3', 'bmb', 1207, 'Dhaka', 'vgnc', '$2b$10$afGlnmoc5l0gO6ZhtfAo4uq0juSiem3b3.lKDTAiZTZUa42AC/hAm', 'uploads\\1728324903667.jpg', '2024-10-07 18:15:03', '01813075310', 0),
+(42, 'TAMIM', 'HAQUE', 'tamim@gmail.com', 'male', 'muxa', 'sul', 2121146042, '2024-09-01', 'O+', 3, '3', 'mirpur', 1207, 'Dhaka', 'bng', '$2b$10$Xkm.IJ0XXqv8Brizv6SAXu9RFO4SxbF7QIBsWSJxQYfChJr9CJDpu', 'uploads\\1728838202268.jpg', '2024-10-13 16:50:02', '01813074310', 1),
+(43, 'TAMIM', 'HASAN', '@gmail.com', 'male', 'MUXA', 'SUTANA', 2121146642, '2024-09-03', 'O+', 3, '2', 'MIRPUR', 1208, 'Dhaka', 'BANGLADESH', '$2b$10$3HLFfBw9mmem1C8OfvyUN.jUIjB27ChP1tdbz3DIS5XqWDNZKo1y6', 'uploads\\1728929814184.jpg', '2024-10-14 18:16:54', '01929487008', 0),
+(46, 'TAMIM', 'AHMED', 'tamim@gmail.com', 'male', 'MUXA', 'HAQUE', 2121146142, '2024-10-28', 'O+', 3, '3', 'ADABOR', 1207, 'Dhaka', 'BNG', '$2b$10$IlfqHKqwqXmgIeddxg7cU.4gzuoO6jKRShYVV8d0UT5hn.hqFdTqW', 'uploads\\1730569985002.jpg', '2024-11-02 17:53:05', '01929487009', 1);
 
 --
 -- Indexes for dumped tables
@@ -160,13 +166,13 @@ ALTER TABLE `administrator`
 -- AUTO_INCREMENT for table `candidate`
 --
 ALTER TABLE `candidate`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `voter`
 --
 ALTER TABLE `voter`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
